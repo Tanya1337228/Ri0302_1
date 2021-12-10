90d800d7cdd02264c28e23485629f103ab2b9564
Lucky-Ded <vitas1pro1@gmail.com>
2021-12-10 13:09:03 +0700

    наши числа

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое число");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
+            int vari = int.Parse(Console.ReadLine());
+            switch (vari)
+            {
+                case 1:
+                    pluss(A, B);
+                    break;
+                case 2:
+                    minus(A, B);
+                    break;
+                case 3:
+                    multy(A, B);
+                    break;
+                case 4:
+                    devide(A, B);
+                    break;
+            }
+        }
+    }
+}
